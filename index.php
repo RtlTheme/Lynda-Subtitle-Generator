6614b72f99109a65f27504e61c92da937cf8a2eb
Hashem Qolami <hashem@qolami.com>
2013-02-07 19:08:24 +0330

    Zip on the fly v0.7.0

--- index.php
+++ index.php
@@ -3,15 +3,15 @@
  * Lynda Subtitle Generator - PHP application
  * https://github.com/qolami/Lynda-Subtitle-Generator
  * Copyright 2013 Hashem Qolami <hashem@qolami.com>
- * Version 0.6.2
+ * Version 0.7.0
  * Released under the MIT and GPL licenses.
 */
 
 # Path to subtitle folder
-define('DIR', './subtitle');
+define('DIR', 'subtitles');
 
 # App version
-$version = '0.6.2';
+$version = '0.7.0';
 
 # Custom output
 function e($msg, $err=FALSE)
@@ -24,23 +24,32 @@ function e($msg, $err=FALSE)
 # Get transcript url
 $url = $_GET['url'] or e('Insert a URL to grab transcript.', TRUE);
 
-# No time limit
-set_time_limit(0);
+// # API
+// $api = !!$_GET['api'] or FALSE;
 
-# Load HTML DOM library
+// # No time limit
+set_time_limit(0);
+@ini_set("max_execution_time", 0);
+
+// # Load libraries
 include 'lib/simple_html_dom.php';
+include 'lib/CreateZipFile.inc.php';
 
 
 function get_path($url)
 {
+	$arr = array('root' => rtrim(DIR, '/'));
+
 	if ( @preg_match('#^https?://(.*)#i', $url, $param) ) {
 		$param = explode('/', $param[1]);
-		return rtrim(DIR, '/')."/$param[1]-$param[2]";
+		$arr['course'] = "$param[1]-$param[2]";
 
 	} else { # local address
 		$param = end(explode('/', $url));
-		return rtrim(DIR, '/').'/'.basename($param, strrchr($param, '.'));
+		$arr['course'] = basename($param, strrchr($param, '.'));
 	}
+	$arr['full'] = $arr['root'].'/'.$arr['course'];
+	return $arr;
 }
 
 function str_pure($str)
@@ -48,23 +57,15 @@ function str_pure($str)
 	return trim(str_replace(array('\\','/',':','*','?','"','<','>','|'), '', $str));
 }
 
-function to_dir($path)
-{
-	# Make directory if not exists. set permission to 0777.
-	is_dir($path) and @chmod($path, 0777) or mkdir($path, 0777, true);
-	return $path;
-}
-
 function to_srt($data, $path, $title)
 {
+	global $zip;
+
 	if (function_exists('mb_convert_encoding')) {
 		$data = mb_convert_encoding($data, 'UTF-8', 'HTML-ENTITIES');
 	}
 	
-	file_put_contents(rtrim($path, '/').'/'.$title.'.srt', $data) or e('Unable to write the data.', TRUE);
-
-	# Change permission of folder according to security issues.
-	@chmod($path, 0755);
+	$zip->addFile($data, rtrim($path, '/').'/'.$title.'.srt');
 }
 
 function parse_chapter_name($ch, $i)
@@ -75,12 +76,15 @@ function parse_chapter_name($ch, $i)
 
 function process_chapter($e, $path, $chno)
 {
+	global $zip;
+
 	$chapter = $e->find('span.chTitle', 0)->plaintext;
 	$sections = $e->find('tr.showToggleDeltails');
 
 	$chapter = parse_chapter_name($chapter, $chno);
 
-	$dir = to_dir( $path .'/'. str_pure($chapter) );
+	$dir = $path .'/'. str_pure($chapter);
+	$zip->addDirectory($dir);
 
 	$j = 1;
 	foreach ($sections as $section) {
@@ -92,7 +96,7 @@ function process_chapter($e, $path, $chno)
 		for ($i = 0; $i < count($rows)-1;) {
 			$start = $rows[$i]->plaintext;
 			$end = $rows[$i+1]->plaintext;
-			$text = $rows[$i]->next_sibling()->plaintext;
+			$text = trim($rows[$i]->next_sibling()->plaintext);
 			$i++;
 			$sub .= "$i
 00:{$start},000 --> 00:{$end},000
@@ -100,29 +104,36 @@ function process_chapter($e, $path, $chno)
 
 ";
 		}
-
+		
 		to_srt( $sub, $dir, str_pure($title) );
 		$j++;
 	}
+}
+
+function get_file_address($filename)
+{
+		$pos = strpos($_SERVER['REQUEST_URI'], $_SERVER['QUERY_STRING']);
+		return substr($_SERVER['REQUEST_URI'], 0, $pos - 1) . $filename;
 }
 
 ############################################################
 ######                   Controller                   ######
 ############################################################
 
-# Make an instance
+# Make instances
 $html = new simple_html_dom();
+$zip = new CreateZipFile;
 
 # Load the DOM
 $html->load_file($url);
 
-$chs = $html->find('td.tChap');
+$chs = $html->find('td.tChap') or e("Unable to find chapters on: <strong><i>$url</i></strong>", TRUE);
 
 # Course path
-$cpath = get_path($url);
+$path = get_path($url);
 
 for ($i=0; $i<count($chs); $i++) {
-	process_chapter($chs[$i], $cpath, $i);
+	process_chapter($chs[$i], $path['course'], $i);
 }
 
 # Clear DOM object
@@ -131,5 +142,15 @@ $html->clear();
 # Free memory
 unset($html);
 
-e("Subtitles have been generated successfully!<br>Located at: <strong>$cpath</strong>");
-?>
+$zip_file = $path['full'].'.zip';
+
+# Write to file, force binary mode
+$file = fopen($zip_file, 'wb');
+$out = fwrite($file, $zip->getZippedfile());
+fclose($file);
+
+// $zip->forceDownload($zip_file);
+// @unlink($zip_file);
+
+$link = get_file_address($zip_file);
+e("Subtitles have been generated successfully!<br>Located at: <a href='$link'><strong>$zip_file</strong></a>");
